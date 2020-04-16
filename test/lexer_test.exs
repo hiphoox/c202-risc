565149b4c1dfdfce8303f8ca25fc8bffe99391cd
Cesar <cesar.yt2015@gmail.com>
2020-04-15 23:18:24 -0500

    Pruebas agregadas de la segunda entrega

--- test/lexer_test.exs
+++ test/lexer_test.exs
@@ -149,6 +149,253 @@ defmodule LexerTest do
     s_code = Sanitizer.sanitize_source(code)
     assert Lexer.scan_words(s_code) == state[:tokens]
   end
+  #Entrega_2
+  test "negation operational"do
+    code ="""
+    int main(){
+      return -2;
+    }
+    """
+    s_code = Sanitizer.sanitize_source(code)
+    assert Lexer.scan_words(s_code) ==
+    [
+      :int_keyword,
+      :main_keyword,
+      :open_paren,
+      :close_paren,
+      :open_brace,
+      :return_keyword,
+      :negation_operation,
+      {:constant, 2},
+      :semicolon,
+      :close_brace
+    ]
+    end
+  test "bitwise operation"do
+    code ="""
+    int main(){
+     return ~2;
+    }
+    """
+    s_code = Sanitizer.sanitize_source(code)
+    assert Lexer.scan_words(s_code) ==
+    [
+    :int_keyword,
+    :main_keyword,
+    :open_paren,
+    :close_paren,
+    :open_brace,
+    :return_keyword,
+    :bitwise_operation,
+    {:constant, 2},
+    :semicolon,
+    :close_brace
+    ]
+  end
+  test "negation logical"do
+    code ="""
+    int main(){
+    return !2;
+    }
+    """
+    s_code = Sanitizer.sanitize_source(code)
+  assert Lexer.scan_words(s_code) ==
+    [
+    :int_keyword,
+    :main_keyword,
+    :open_paren,
+    :close_paren,
+    :open_brace,
+    :return_keyword,
+    :negation_logical,
+    {:constant, 2},
+    :semicolon,
+    :close_brace
+    ]
+  end
+  test "BO & NO"do
+    code ="""
+      int main(){
+      return ~-2;
+      }
+      """
+  s_code = Sanitizer.sanitize_source(code)
+  assert Lexer.scan_words(s_code) ==
+   [
+    :int_keyword,
+    :main_keyword,
+    :open_paren,
+    :close_paren,
+    :open_brace,
+    :return_keyword,
+    :bitwise_operation,
+    :negation_operation,
+    {:constant, 2},
+    :semicolon,
+    :close_brace
+    ]
+  end
+   test "Multi digit negation"do
+   code ="""
+     int main(){
+      return ~-1000;
+     }
+     """
+     s_code = Sanitizer.sanitize_source(code)
+     assert Lexer.scan_words(s_code) ==
+    [
+      :int_keyword,
+      :main_keyword,
+      :open_paren,
+      :close_paren,
+      :open_brace,
+      :return_keyword,
+      :bitwise_operation,
+      :negation_operation,
+      {:constant, 1000},
+      :semicolon,
+      :close_brace
+    ]
+  end
+  test "NL & BO"do
+  code =
+  """
+    int main(){
+     return !~1000;
+      }
+  """
+s_code = Sanitizer.sanitize_source(code)
+assert Lexer.scan_words(s_code) ==
+     [
+      :int_keyword,
+      :main_keyword,
+      :open_paren,
+      :close_paren,
+      :open_brace,
+      :return_keyword,
+      :negation_logical,
+      :bitwise_operation,
+      {:constant, 1000},
+      :semicolon,
+      :close_brace
+      ]
+  end
+test "NL & NO"do
+code ="""
+    int main(){
+    return !-100;
+     }
+    """
+    s_code = Sanitizer.sanitize_source(code)
+ assert Lexer.scan_words(s_code) ==
+    [
+    :int_keyword,
+    :main_keyword,
+    :open_paren,
+    :close_paren,
+    :open_brace,
+    :return_keyword,
+    :negation_logical,
+    :negation_operation,
+    {:constant, 100},
+    :semicolon,
+    :close_brace
+    ]
+  end
+test "Doble Negation "do
+ code ="""
+  int main(){
+  return --100;
+  }
+  """
+ s_code = Sanitizer.sanitize_source(code)
+ assert Lexer.scan_words(s_code) ==
+    [
+    :int_keyword,
+    :main_keyword,
+    :open_paren,
+    :close_paren,
+    :open_brace,
+    :return_keyword,
+    :negation_operation,
+    :negation_operation,
+    {:constant, 100},
+    :semicolon,
+    :close_brace
+    ]
+ end
+ test "Doble Negation Logical "do
+  code ="""
+   int main(){
+   return !!100;
+   }
+   """
+  s_code = Sanitizer.sanitize_source(code)
+  assert Lexer.scan_words(s_code) ==
+     [
+     :int_keyword,
+     :main_keyword,
+     :open_paren,
+     :close_paren,
+     :open_brace,
+     :return_keyword,
+     :negation_logical,
+     :negation_logical,
+     {:constant, 100},
+     :semicolon,
+     :close_brace
+     ]
+  end
+  test "Doble Bitwise Operation "do
+    code ="""
+     int main(){
+     return ~~100;
+     }
+     """
+    s_code = Sanitizer.sanitize_source(code)
+    assert Lexer.scan_words(s_code) ==
+       [
+       :int_keyword,
+       :main_keyword,
+       :open_paren,
+       :close_paren,
+       :open_brace,
+       :return_keyword,
+       :bitwise_operation,
+       :bitwise_operation,
+       {:constant, 100},
+       :semicolon,
+       :close_brace
+       ]
+    end
+test "Multiple Negation "do
+code ="""
+      int main(){
+      return !~-!~-100;
+      }
+      """
+s_code = Sanitizer.sanitize_source(code)
+assert Lexer.scan_words(s_code) ==
+  [
+  :int_keyword,
+  :main_keyword,
+  :open_paren,
+  :close_paren,
+  :open_brace,
+  :return_keyword,
+  :negation_logical,
+  :bitwise_operation,
+  :negation_operation,
+  :negation_logical,
+  :bitwise_operation,
+  :negation_operation,
+  {:constant, 100},
+  :semicolon,
+  :close_brace
+  ]
+end
+
+
 
 
   # tests to fail
