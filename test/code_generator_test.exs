4f95426fa3e39ba70afff3cbe2892b321f3a75cb
Cesar <cesar.yt2015@gmail.com>
2020-05-27 18:49:42 -0500

    pruebas para el code generator entrega 2 añadidas

--- test/code_generator_test.exs
+++ test/code_generator_test.exs
@@ -20,6 +20,24 @@ defmodule CodeGeneratorTest do
     p_code =  Parser.parse_program(s_code)
     assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $100,%ebx\n    movl    %ebx, %eax\n    ret\n"
   end
+  test "parser negation operational" do
+    code = ["int", "main(){","\r\n", "return", "-2;","\r\n","}"]
+    s_code = Lexer.scan_words(code)
+    p_code =  Parser.parse_program(s_code)
+    assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $2,%ebx\nneg %ebx\n\n    movl    %ebx, %eax\n    ret\n"
+  end
+  test "parser bitwise operation" do
+    code = ["int", "main(){","\r\n", "return", "~2;","\r\n","}"]
+    s_code = Lexer.scan_words(code)
+    p_code =  Parser.parse_program(s_code)
+    assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $2,%ebx\nnot %ebx\n\n    movl    %ebx, %eax\n    ret\n"
+  end
+  test "parser logical negation" do
+    code = ["int", "main(){","\r\n", "return", "!2;","\r\n","}"]
+    s_code = Lexer.scan_words(code)
+    p_code =  Parser.parse_program(s_code)
+    assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $2,%ebx\r\n    movl $0,%eax\r\n    cmpl %eax,%ebx\r\n    movl $0,%ebx\r\n    sete %dl\n    movl    %ebx, %eax\n    ret\n"
+  end
 
 
 end
