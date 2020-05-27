9c5a2a357cc9e313c50b6d689d1ead3d4fae57ca
Cesar <cesar.yt2015@gmail.com>
2020-05-26 21:35:48 -0500

    pruebas para code_generator entrega 1 añadidas

---
+++ test/code_generator_test.exs
@@ -0,0 +1,25 @@
+defmodule CodeGeneratorTest do
+  use ExUnit.Case
+  doctest CodeGenerator
+  #Nuevas Test del parser entrega 1 con modificaciones respecto a entrega 3
+  test "parser return 2" do
+    code = ["int", "main(){","\r\n", "return", "2;","\r\n","}"]
+    s_code = Lexer.scan_words(code)
+    p_code =  Parser.parse_program(s_code)
+    assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $2,%ebx\n    movl    %ebx, %eax\n    ret\n"
+  end
+  test "parser return 0" do
+    code = ["int", "main(){","\r\n", "return", "0;","\r\n","}"]
+    s_code = Lexer.scan_words(code)
+    p_code =  Parser.parse_program(s_code)
+    assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $0,%ebx\n    movl    %ebx, %eax\n    ret\n"
+  end
+  test "parser return multidigit" do
+    code = ["int", "main(){","\r\n", "return", "100;","\r\n","}"]
+    s_code = Lexer.scan_words(code)
+    p_code =  Parser.parse_program(s_code)
+    assert CodeGenerator.generate_code(p_code) == "    .section        .text\n    .p2align        4, 0x90\n    .globl  _main         ## -- Begin function main\n_main:                    ## @main\nmovl $100,%ebx\n    movl    %ebx, %eax\n    ret\n"
+  end
+
+
+end
